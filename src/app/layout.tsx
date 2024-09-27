cac892396ac917051e30dff0927f248e772dc3d7
hovanhoa <hovanhoaa1@gmail.com>
2024-09-27 14:31:50 +0700

    feat: remove gg analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { BASE_URL, NAME } from '@/constants'
 import { Analytics } from '@vercel/analytics/react'
-import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -46,9 +45,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="scroll-smooth">
-            <head>
-                <GoogleAnalytics />
-            </head>
             <body className={inter.className}>{children}</body>
             <Analytics />
         </html>
