8be8df0dadc1afb18a1313a79fc927389ddcfb2c
hovanhoa <hovanhoaa1@gmail.com>
2024-12-31 14:21:00 +0700

    feat: update underline

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,19 +8,19 @@ export default function Navbar() {
             <nav className="relative mx-auto max-w-xl">
                 <ul className="flex items-center space-x-6 text-base text-black cursor-pointer">
                     <Link
-                        className="hover:text-slate-400 transition duration-300 ease-in-out"
+                        className="hover:underline hover:decoration-wavy hover:underline-offset-8 transition duration-300 ease-in-out"
                         href={BASE_URL}
                     >
                         Home
                     </Link>
                     <Link
-                        className="hover:text-slate-400 transition duration-300 ease-in-out"
+                        className="hover:underline hover:decoration-wavy hover:underline-offset-8 transition duration-300 ease-in-out"
                         href={INSIGHT_URL}
                     >
                         Insight
                     </Link>
                     <Link
-                        className="hover:text-slate-400 transition duration-300 ease-in-out"
+                        className="hover:underline hover:decoration-wavy hover:underline-offset-8 transition duration-300 ease-in-out"
                         href={GALLERY_URL}
                     >
                         Gallery
