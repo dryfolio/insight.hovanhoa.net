d59d6f78dbae58d94a3ca4d185ab0a2c8f4e070f
hovanhoa <hovanhoaa1@gmail.com>
2026-04-03 09:35:09 +0700

    Add music navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,4 @@
-import { INSIGHT_URL, BASE_URL, GALLERY_URL } from '@/constants'
+import {BASE_URL, GALLERY_URL, GITHUB, INSIGHT_URL, LINKEDIN, MUSIC_URL} from '@/constants'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <header>
             <nav className="relative mx-auto max-w-xl">
-                <ul className="flex items-center space-x-3 sm:space-x-6 text-sm sm:text-base text-black cursor-pointer">
+                <ul className="flex items-center space-x-6 text-base text-black cursor-pointer">
                     <Link
                         className="hover:underline hover:decoration-wavy hover:underline-offset-8 transition duration-300 ease-in-out"
                         href={BASE_URL}
@@ -24,6 +24,12 @@ export default function Navbar() {
                         href={GALLERY_URL}
                     >
                         Gallery
+                    </Link>
+                    <Link
+                        className="hover:underline hover:decoration-wavy hover:underline-offset-8 transition duration-300 ease-in-out"
+                        href={MUSIC_URL}
+                    >
+                        Music
                     </Link>
                 </ul>
             </nav>
